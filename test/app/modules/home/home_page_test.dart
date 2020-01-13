db1ee716768f1f1d53853ad4e868b2bc70e090ad
JP <sr.joaovirgili@gmail.com>
2020-01-13 15:26:03 -0300

    Removed unused imports

--- test/app/modules/home/home_page_test.dart
+++ test/app/modules/home/home_page_test.dart
@@ -1,7 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_modular/flutter_modular_test.dart';
-
-import 'package:mobx_shop_cart/app/modules/home/home_page.dart';
 
 main() {
   testWidgets('HomePage has title', (WidgetTester tester) async {
--- test/app/shared/components/cart_item/cart_item_widget_test.dart
+++ test/app/shared/components/cart_item/cart_item_widget_test.dart
@@ -1,7 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_modular/flutter_modular_test.dart';
-
-import 'package:mobx_shop_cart/app/shared/components/cart_item/cart_item_widget.dart';
 
 main() {
   testWidgets('CartItemWidget has message', (WidgetTester tester) async {
